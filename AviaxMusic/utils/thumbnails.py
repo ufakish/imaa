a3fea7a7830cc04f50ff3d819dbc1e5c9a0fe459
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-10 12:14:17 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -235,7 +235,7 @@ async def gen_thumb(videoid: str):
         
         play_icos = Image.open("AviaxMusic/assets/play_icos.png")
         play_icos = play_icos.resize((470, 50))
-        background.paste(play_icos, (130, 170), play_icos)
+        background.paste(play_icos, (100, 400), play_icos)
         
 
         play_iconmm = Image.open("AviaxMusic/assets/play_icon.png")
