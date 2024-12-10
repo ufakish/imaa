c66df419ccd1f30d60b4093d682cd08916f89f1a
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-10 11:31:08 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -186,8 +186,8 @@ async def gen_thumb(videoid: str):
         title_font = ImageFont.truetype("AviaxMusic/assets/font3.ttf", 45)
 
 
-        circle_thumbnail = crop_center_circle(youtube, 400,400 , start_gradient_color)
-        circle_thumbnail = circle_thumbnail.resize((400, 400))
+        circle_thumbnail = crop_center_circle(youtube, 400,20 , start_gradient_color)
+        circle_thumbnail = circle_thumbnail.resize((400, 200))
         circle_position = (120, 160)
         background.paste(circle_thumbnail, circle_position, circle_thumbnail)
 
