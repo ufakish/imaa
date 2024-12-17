84bacff8518c5156014e901ac18d8298a345a9ef
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-17 21:22:59 +0530

    Update play.py

--- AviaxMusic/plugins/play/play.py
+++ AviaxMusic/plugins/play/play.py
@@ -29,7 +29,7 @@ from config import BANNED_USERS, lyrical
     filters.command(
         [
             "play",
-            "p", 
+            "pl", 
             "gana", 
             "song", 
             "vplay",
